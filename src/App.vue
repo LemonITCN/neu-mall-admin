5d9263b6a108d99c680bf48a2c22c2412a639b3d
LemonITCN <liuri@lemonit.cn>
2021-12-29 17:48:34 +0800

    完成了登录

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,239 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="layout" id="app">
+    <el-container v-if="showMenu" class="container">
+      <el-aside class="aside">
+        <div class="head">
+          <div>
+            <img src="https://s.weituibao.com/1582958061265/mlogo.png" alt="logo">
+            <span>vue2 admin</span>
+          </div>
+        </div>
+        <div class="line"/>
+        <el-menu
+          :default-openeds="defaultOpen"
+          background-color="#222832"
+          text-color="#fff"
+          :router="true"
+          :default-active='currentPath'
+        >
+          <el-submenu index="1">
+            <template #title>
+              <span>Dashboard</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/introduce"><i class="el-icon-data-line"/>系统介绍</el-menu-item>
+              <el-menu-item index="/dashboard"><i class="el-icon-odometer"/>Dashboard</el-menu-item>
+              <el-menu-item index="/add"><i class="el-icon-plus"/>添加商品</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template #title>
+              <span>首页配置</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/swiper"><i class="el-icon-picture"/>轮播图配置</el-menu-item>
+              <el-menu-item index="/hot"><i class="el-icon-star-on"/>热销商品配置</el-menu-item>
+              <el-menu-item index="/new"><i class="el-icon-sell"/>新品上线配置</el-menu-item>
+              <el-menu-item index="/recommend"><i class="el-icon-thumb"/>为你推荐配置</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template #title>
+              <span>模块管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/category"><i class="el-icon-menu"/>分类管理</el-menu-item>
+              <el-menu-item index="/good"><i class="el-icon-s-goods"/>商品管理</el-menu-item>
+              <el-menu-item index="/guest"><i class="el-icon-user-solid"/>会员管理</el-menu-item>
+              <el-menu-item index="/order"><i class="el-icon-s-order"/>订单管理</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template #title>
+              <span>系统管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/account"><i class="el-icon-lock"/>修改密码</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container class="content">
+        <Header/>
+        <div class="main">
+          <router-view/>
+        </div>
+        <Footer/>
+      </el-container>
+    </el-container>
+    <el-container v-else class="container">
+      <router-view/>
+    </el-container>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import { localGet, pathMap } from '@/utils'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  },
+  data () {
+    return {
+      noMenu: ['/login'],
+      defaultOpen: ['1', '2', '3', '4'],
+      showMenu: true,
+      currentPath: '/dashboard',
+      count: {
+        number: 1
+      }
+    }
+  },
+  mounted () {
+    if (!localGet('token')) {
+      this.showMenu = false
+    }
+    // 监听浏览器原生回退事件
+    if (window.history && window.history.pushState) {
+      history.pushState(null, null, document.URL)
+      window.addEventListener('popstate', () => {
+        console.log('hahahah', localGet('token'))
+        if (!localGet('token')) {
+          this.showMenu = false
+        }
+      }, false)
+    }
+  },
+  methods: {
+    unwatch (to, from, next) {
+      if (to.path === '/login') {
+        // 如果路径是 /login 则正常执行
+        next()
+      } else {
+        // 如果不是 /login，判断是否有 token
+        if (!localGet('token')) {
+          // 如果没有，则跳至登录页面
+          next({ path: '/login' })
+        } else {
+          // 否则继续执行
+          next()
+        }
+      }
+      this.showMenu = !this.noMenu.includes(to.path)
+      this.currentPath = to.path
+      document.title = pathMap[to.name]
+    }
+  },
+  destroyed () {
+    this.unwatch()
+  }
+}
+</script>
+
+<style scoped>
+.layout {
+  min-height: 100vh;
+  background-color: #ffffff;
+}
+
+.container {
+  height: 100vh;
+}
+
+.aside {
+  width: 200px !important;
+  background-color: #222832;
+  overflow: hidden;
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  overflow: -moz-scrollbars-none;
+}
+
+.aside::-webkit-scrollbar {
+  display: none;
+}
+
+.head {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+}
+
+.head > div {
+  display: flex;
+  align-items: center;
+}
+
+.head img {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+}
+
+.head span {
+  font-size: 20px;
+  color: #ffffff;
+}
+
+.line {
+  border-top: 1px solid hsla(0, 0%, 100%, .05);
+  border-bottom: 1px solid rgba(0, 0, 0, .2);
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  overflow: hidden;
+}
+
+.main {
+  height: calc(100vh - 100px);
+  overflow: auto;
+  padding: 10px;
+}
+</style>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.el-menu {
+  border-right: none !important;
+}
+
+.el-submenu {
+  border-top: 1px solid hsla(0, 0%, 100%, .05);
+  border-bottom: 1px solid rgba(0, 0, 0, .2);
+}
+
+.el-submenu:first-child {
+  border-top: none;
+}
+
+.el-submenu [class^="el-icon-"] {
+  vertical-align: -1px !important;
+}
+
+a {
+  color: #409eff;
+  text-decoration: none;
+}
+
+.el-pagination {
   text-align: center;
-  color: #2c3e50;
+  margin-top: 20px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.el-popper__arrow {
+  display: none;
 }
 </style>
